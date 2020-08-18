af87ceea3dc364718a59f5e6b88031377dee7bdf
farhadahmed3020 <67541639+farhadahmed3020@users.noreply.github.com>
2020-08-18 23:04:46 +0600

    add js

---
+++ conditional.js
@@ -0,0 +1,26 @@
+var pantPrice =200;
+//use not equal(!=) ,equal(=), comparing (==) sing---//
+if (pantPrice ==250) {
+    console.log("I will sale this");
+} else {
+    console.log("No over price");
+}
+// second condition 2 condition  are true//
+var jobPaiso = true;
+var savingAmount = 500000;
+
+if (jobPaiso == true && savingAmount>200000) {
+    console.log("get marry");
+} else {
+    console.log("sorry you not qualify for marry");
+}
+
+// second condition 1 condition  are true//
+var jobPaiso = true;
+var savingAmount = 50000;
+
+if (jobPaiso == true || savingAmount>200000) {
+    console.log("get marry");
+} else {
+    console.log("sorry you not qualify for marry");
+}
---
+++ pogramming.js
@@ -0,0 +1,31 @@
+var bananaPrice = 5;
+console.log("price" ,bananaPrice);
+
+//type of variable//
+
+var name = "farhad";
+console.log("string",name);
+
+ var number = 56 ;
+ console.log("value" ,number);
+// true & false is boolean types variable
+ var isHot = true;
+ var isRich = false;
+ console.log(typeof isHot);
+
+ var firstName = "farhad";
+ var lastName = "ahmed";
+ var fullName = firstName + " " + lastName;
+ console.log(fullName);
+ var number1 = 25;
+ var number2 = 244.499;
+ var result = parseInt(number1 + number2);
+ console.log(result);
+var result1 = Math.ceil(number2);
+console.log("celing",result1); 
+var result2 = Math.round(number2);
+console.log("round", result2);
+// random number gain students lotare counting formula//
+var number3 = Math.random()*1000;
+var result3 = Math.round(number3);
+console.log("randomNumber", result3);
