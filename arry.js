3faf955aa1f5f546ccc1c5782f810556fd3fd741
farhadahmed3020 <67541639+farhadahmed3020@users.noreply.github.com>
2020-08-19 00:43:55 +0600

    add code

---
+++ arry.js
@@ -0,0 +1,23 @@
+var tremMate = ['palam','kamal','jamal','samal'];
+
+var name = tremMate[0];
+
+tremMate[0] = 'pamal';
+
+var position = tremMate.indexOf('jamal');
+//var totalLength = tremMate.length
+tremMate .push('damal','jumal');
+
+//console.log(tremMate);
+
+console.log(position);
+
+//console.log(totalLength);
+// arry push //
+
+
+
+console.log(tremMate.length);
+
+
+
---
+++ loop-apply.js
@@ -0,0 +1,10 @@
+var nums = ['palam','kamal','jamal','samal'];
+// while (num <15) {
+//     console.log(num);
+//     num++;
+// }
+for (var i = 0; i <=nums.length; i++) {
+    var element = nums[i];
+    console.log(element);
+    
+}
