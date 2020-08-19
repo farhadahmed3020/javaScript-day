f0455a6af8ed9783f417484d834f78699c5f0c87
farhadahmed3020 <67541639+farhadahmed3020@users.noreply.github.com>
2020-08-19 20:48:08 +0600

    add new

---
+++ function.js
@@ -0,0 +1,22 @@
+// function loveBaby (){
+// console.log("love you baby");
+// console.log('miss you baby');
+// }
+// loveBaby();
+// function doubleIt(num1) {
+//     var result = num*2;
+//     return result;
+// }
+// var first = doubleIt(20);
+// var second = doubleIt(60);
+// var total = first+ second;
+// console.log(total);
+
+
+function add(num1,num2,num3) {
+    var sum = num1+ num2+ num3;
+    return sum;
+    
+}
+var result1 = add(49,89,45);
+console.log(result1);
---
+++ object.js
@@ -0,0 +1,4 @@
+var student = { id:121,phone:1745,name:"Abir"};
+var student2 = {id: 131,phone: 1675,name:"Eva" };
+console.log(student.phone);
+console.log(student2.name);
